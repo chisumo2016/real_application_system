03430deff347862e51842250c3177832a0568109
Bernard Chisumo <bchisumo74@yahoo.com>
2017-02-19 23:34:59 +0000

    Mass Assigment has been rectified in Post Model -Works fine

--- app/Post.php
+++ app/Post.php
@@ -6,7 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    // The attributes that are mass assignable
+
+    protected $fillable = [
+        'title', 'content', 'featured', 'category_id',
+    ];
 
    public  function  Categories()
    {
