eac533344df7e93d3cad85fc09307f0eeb4f3056
Bernard Chisumo <bchisumo74@yahoo.com>
2017-02-20 07:45:21 +0000

    Soft deletes table setup

--- app/Post.php
+++ app/Post.php
@@ -3,14 +3,19 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 
 class Post extends Model
 {
+    use SoftDelete;
     // The attributes that are mass assignable
 
     protected $fillable = [
         'title', 'content', 'featured', 'category_id',
     ];
+
+    protected $dates = ['deleted_at'];
 
    public  function  Categories()
    {
