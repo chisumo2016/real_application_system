bfe221f1969bd6ed17c031b15d1739d85234e04d
Bernard Chisumo <bchisumo74@yahoo.com>
2017-02-21 11:52:00 +0000

    Profiles for users and relation has been set up

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,11 +12,21 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         //Seeding Defaulf Adminstrator]]
-        App\User::create([
+       $user = App\User::create([
             'name'      => 'jumbura juma',
             'email'     => 'jumbura@gmail.com',
-            'password'  =>  bcrypt('password')
+            'password'  =>  bcrypt('password'),
+            'admin'     =>  1
 
+        ]);
+
+        // Profile for the above use
+        $user = App\Profile::create([
+            'user_id' => $user->id,
+            'avatar'  =>'uploads/avatars/1.png',
+            'about'   =>'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis cupiditate harum maiores, neque porro voluptatibus. Accusantium architecto deleniti, ea exercitationem harum magni minima non perferendis porro quas repudiandae sint voluptas.',
+            'facebook'=>'facebook.com',
+            'youtube'=>'youtube'
         ]);
     }
 }
