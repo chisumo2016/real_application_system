074df489d02dddcbe8eae20956a9f6eef98ce462
Bernard Chisumo <bchisumo74@yahoo.com>
2017-02-27 21:16:12 +0000

    Dashboard setup

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Category;
+use App\Post;
+use App\User;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -14,6 +17,15 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $posts_count =Post::all()->count();;
+        $users_count = User::all()->count();;
+        $categories_count  = Category::all()->count();;
+        $trashed_count = Post::onlyTrashed()->get()->count();
+
+
+
+
+        return view('admin.dashboard', compact('posts_count','trashed_count', 'users_count','categories_count'));
+
     }
 }
